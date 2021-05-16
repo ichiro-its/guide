678b66df2ca4b086c14d163e0a9251ab79186553
Alfi Maulana <alfi.maulana.f@gmail.com>
2021-05-16 20:44:55 +0700

    feat: use sphinx with read the docs theme

---
+++ conf.py
@@ -0,0 +1,28 @@
+import sphinx_rtd_theme
+
+project = 'ICHIRO ITS Guide'
+
+copyright = '2019-2021, ICHIRO ITS'
+author = 'ICHIRO ITS'
+
+version = ''
+release = ''
+
+extensions = [
+    'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+language = None
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = None
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+htmlhelp_basename = 'guidedoc'
